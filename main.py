e43f257a41e8fa269f60c96d0fecab3fe7b9640e
cjones1047 <cjmj1047@gmail.com>
2022-12-03 06:30:33 -0600

    New list is created for each name in invited_names.txt, first index of list is replaced with header that includes name of letter's recipient

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 # TODO: Create a letter using starting_letter.txt
 # for each name in invited_names.txt
 
+cleaned_names_list = []
+
 with open("Input/Names/invited_names.txt", mode="r") as all_names:
     names_list = all_names.readlines()
-    cleaned_names_list = []
     for name in names_list:
         cleaned_names_list.append(name.strip())
     print(cleaned_names_list)
 
 # Replace the [name] placeholder with the actual name.
 # Save the letters in the folder "ReadyToSend".
-    
-# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+for name in cleaned_names_list:
+    named_letter_list = []
+    with open("Input/Letters/starting_letter.txt", mode="r") as letter_template:
+        template_list = letter_template.readlines()
+        template_list[0] = f"Dear {name},\n"
+        for line in template_list:
+            named_letter_list.append(line)
+    print(named_letter_list)
+    #     # with open(f"Output/ReadyToSend/{name}.txt", mode="w") as new_letter:
+    #     #     for l
