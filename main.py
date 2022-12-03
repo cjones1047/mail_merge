45424a9025b737a5b6386b90805081428fd025ef
cjones1047 <cjmj1047@gmail.com>
2022-12-03 05:47:07 -0600

    All names extracted from invited_names.txt, new line syntax removed from each item and appended to new list so new list contains all names

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
+# TODO: Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+
+with open("Input/Names/invited_names.txt", mode="r") as all_names:
+    names_list = all_names.readlines()
+    cleaned_names_list = []
+    for name in names_list:
+        cleaned_names_list.append(name.strip())
+    print(cleaned_names_list)
+
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
     
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
